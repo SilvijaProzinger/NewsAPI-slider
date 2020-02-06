9162aa9825b81f10ffbcbd0fc469fb073f3b37fc
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-02-06 20:02:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let key = 'f49c9a8b86754d37b682f8d6120ac075'
+let searchTerm = 'netflix'
+let url = `https://newsapi.org/v2/everything?` +
+          `q=${searchTerm}&` +
+          `from=2020-02-06&` +
+          `sortBy=popularity&` +
+          `pageSize=5&` +
+          `apiKey=${key}`;
+
+$('#searchButton').on('click', function(e){
+	e.preventDefault()
+
+	var req = new Request(url);
+
+	if (searchTerm !== ''){
+		fetchNews(req)
+	}
+
+	//to change later
+	else {
+		alert('Please enter a search term and try again')
+	}
+
+})
+
+const fetchNews = (req) => {
+	$.getJSON(url)
+		.done(function(data){
+			console.log(data);
+			let output = ''
+			$.each(data.articles, function(index, article){
+			    output += `
+			      <article class="news">
+			        <img src="${article.urlToImage}" alt="image article" class="news-image"/>	        	        
+					<div class="news-content">
+						<h2 class="news-title">${article.title}</h2>
+						<h4 class="news-author">${article.author}</h4>
+						<h3 class="news-text">${article.content}</h3>
+						<a href="${article.url}">Pročitaj članak</a>
+					</div>
+				    </article>`;
+			  });
+	 		$('#article').html(output);
+		})
+
+		.fail(function(jqxhr, textStatus, error){
+			let err = textStatus + ", " + error;
+			console.log( "Request Failed: " + err );
+		});
+}
